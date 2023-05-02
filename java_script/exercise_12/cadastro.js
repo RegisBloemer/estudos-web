dbb7ad7a84260904da9491023b00d8781fed560f
Régis Nyland Bloemer <regisnb1@gmail.com>
2023-05-02 10:59:41 -0300

    exercicio finish

--- java_script/exercise_12/cadastro.js
+++ java_script/exercise_12/cadastro.js
@@ -1,35 +1,63 @@
-const arr = [];
-let option = "";
+const imoveis = [];
+let opcao = "";
 
 do {
   option = prompt(
-    "Number of registered properties:"` ${arr.length}\n` +
+    `Number of registered properties:${arr.length}\n` +
       "Cards\n" +
-      cards +
-      "\nChoise once option:\n1 - New property\n2 - Pull a card\n3 - Quit"
+      "\nChoise once option:\n1 - New property\n2 - Show saved property \n3 - Quit"
   );
-  switch (option) {
+  // ...
+  switch (opcao) {
     case "1":
-      let insert = prompt("Enter the name of the property you want to save:");
-      arr.unshift(insert);
-      alert(`The property add is ${insert}`);
+      const imovel = {};
+
+      imovel.proprietario = prompt("Informe o nome do proprietário do imóvel:");
+      imovel.quartos = parseFloat(prompt("Quantos quartos possui o imóvel?"));
+      imovel.banheiros = parseFloat(
+        prompt("Quantos banheiros possui o imóvel?")
+      );
+      imovel.garagem = prompt("O imóvel possui garagem? (Sim/Não)");
+
+      const confirma = confirm(
+        "Salvar este imóvel?\n" +
+          "\nProprietário: " +
+          imovel.proprietario +
+          "\nQuartos: " +
+          imovel.quartos +
+          "\nBanheiros: " +
+          imovel.banheiros +
+          "\nPossui Garagem? " +
+          imovel.garagem
+      );
+
+      if (confirma) {
+        imoveis.push(imovel);
+      }
       break;
 
     case "2":
-      let card = arr.shift();
-      if (!card) {
-        alert("The list of cards is empty!");
-      } else {
-        alert("Card removed is" + card);
+      for (let i = 0; i < imoveis.length; i++) {
+        alert(
+          "Imóvel " +
+            (i + 1) +
+            "\nProprietário: " +
+            imoveis[i].proprietario +
+            "\nQuartos: " +
+            imoveis[i].quartos +
+            "\nBanheiros: " +
+            imoveis[i].banheiros +
+            "\nPossui Garagem? " +
+            imoveis[i].garagem
+        );
       }
       break;
 
     case "3":
-      alert("Program is shutting down");
+      alert("Saindo...");
       break;
-
     default:
-      alert("Please choise an valid option!");
+      alert("Opção inválida!");
       break;
   }
-} while (option !== "3");
+} while (opcao !== "3");
