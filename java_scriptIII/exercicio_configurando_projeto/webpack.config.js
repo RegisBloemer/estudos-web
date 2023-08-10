608e47fbff85569888b7c9684916b2815d90e028
Régis Nyland Bloemer <regisnb1@gmail.com>
2023-08-10 13:26:29 -0300

    javascriptIV

--- java_scriptIII/exercicio_configurando_projeto/webpack.config.js
+++ java_scriptIII/exercicio_configurando_projeto/webpack.config.js
@@ -1,6 +1,15 @@
 // webpack.config.js
 
+const path = require("path");
+
 module.exports = {
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    compress: true,
+    port: 8000,
+  },
   entry: {
     index: "./src/index.js",
   },
